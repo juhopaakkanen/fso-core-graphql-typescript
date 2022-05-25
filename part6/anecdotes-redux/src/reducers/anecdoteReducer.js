4a41f4580571b03c6127f08147a04a468e7a07ce
juhopaakkanen <juho.paakkanen@gmail.com>
2022-05-25 14:48:22 +0300

    Add redux state handling

--- part6/anecdotes-redux/src/reducers/anecdoteReducer.js
+++ part6/anecdotes-redux/src/reducers/anecdoteReducer.js
@@ -20,10 +20,27 @@ const asObject = (anecdote) => {
 const initialState = anecdotesAtStart.map(asObject)
 
 const reducer = (state = initialState, action) => {
-  console.log('state now: ', state)
-  console.log('action', action)
+  switch (action.type) {
+    case 'VOTE':
+      return state.map(a => a.id !== action.data ? a : { ...a, votes: a.votes + 1 })
+    case 'NEW':
+      return state.concat(asObject(action.data))
+    default: return state
+  }
+}
 
-  return state
+export const voteAnecdote = (id) => { 
+  return {
+    type: 'VOTE',
+    data: id
+  }
+}
+
+export const createAnecdote = (anecdote) => {
+  return {
+    type: 'NEW',
+    data: anecdote
+  }
 }
 
 export default reducer
