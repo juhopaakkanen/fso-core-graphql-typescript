22927857be00c6f35a5fcca53af2f32494d5187d
juhopaakkanen <juho.paakkanen@gmail.com>
2022-06-21 16:57:37 +0300

    Add basic functionalities

--- part8/GraphQL_backend/index.js
+++ part8/GraphQL_backend/index.js
@@ -1,4 +1,5 @@
 const { ApolloServer, gql } = require('apollo-server')
+const { v1: uuid } = require('uuid')
 
 let authors = [
   {
@@ -25,16 +26,6 @@ let authors = [
     id: 'afa5b6f3-344d-11e9-a414-719c6709cf3e'
   }
 ]
-
-/*
- * Suomi:
- * Saattaisi olla järkevämpää assosioida kirja ja sen tekijä tallettamalla kirjan yhteyteen tekijän nimen sijaan tekijän id
- * Yksinkertaisuuden vuoksi tallennamme kuitenkin kirjan yhteyteen tekijän nimen
- *
- * English:
- * It might make more sense to associate a book with its author by storing the author's id in the context of the book instead of the author's name
- * However, for simplicity, we will store the author's name in connection with the book
- */
 
 let books = [
   {
@@ -89,12 +80,82 @@ let books = [
 ]
 
 const typeDefs = gql`
+  type Author {
+    name: String!
+    born: Int
+    bookCount: Int!
+    id: ID!
+  }
+
+  type Book {
+    title: String!
+    published: Int!
+    author: String!
+    genres: [String!]!
+    id: ID!
+  }
+
   type Query {
+    authorCount: Int!
+    bookCount: Int!
+    allBooks(author: String, genre: String): [Book!]!
+    allAuthors: [Author!]!
+  }
+
+  type Mutation {
+    addBook(
+      title: String!
+      published: Int!
+      author: String!
+      genres: [String!]!
+    ): Book
+    editAuthor(name: String!, setBornTo: Int!): Author
   }
 `
 
 const resolvers = {
-  Query: {}
+  Query: {
+    authorCount: () => authors.length,
+    bookCount: () => books.length,
+    allBooks: (root, args) => {
+      if (!args.author && !args.genre) {
+        return books
+      } else if (args.author && args.genre) {
+        return books
+          .filter((book) => book.author === args.author)
+          .filter((book) => book.genres.includes(args.genre))
+      } else if (args.author) {
+        return books.filter((book) => book.author === args.author)
+      } else {
+        return books.filter((book) => book.genres.includes(args.genre))
+      }
+    },
+    allAuthors: () => authors
+  },
+  Author: {
+    bookCount: (root) =>
+      books.filter((book) => book.author === root.name).length
+  },
+  Mutation: {
+    addBook: (root, args) => {
+      const book = { ...args, id: uuid() }
+      books = books.concat(book)
+      if (!authors.find((a) => a.name === args.author)) {
+        const author = { name: args.author, id: uuid() }
+        authors = authors.concat(author)
+      }
+      return book
+    },
+    editAuthor: (root, args) => {
+      const author = authors.find((a) => a.name === args.name)
+      if (!author) {
+        return null
+      }
+      const updatedAuthor = { ...author, born: args.setBornTo }
+      authors = authors.map((a) => (a.name !== args.name ? a : updatedAuthor))
+      return updatedAuthor
+    }
+  }
 }
 
 const server = new ApolloServer({
