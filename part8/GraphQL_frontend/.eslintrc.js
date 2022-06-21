77f09dfafca57d5efabbd227e1353d1834d393d2
juhopaakkanen <juho.paakkanen@gmail.com>
2022-06-21 17:12:55 +0300

    Set up formatter and linter

---
+++ part8/GraphQL_frontend/.eslintrc.js
@@ -0,0 +1,35 @@
+// eslint-disable-next-line no-undef
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+    'jest/globals': true
+  },
+  extends: ['eslint:recommended', 'plugin:react/recommended', 'prettier'],
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true
+    },
+    ecmaVersion: 'latest',
+    sourceType: 'module'
+  },
+  plugins: ['react', 'jest'],
+  rules: {
+    indent: ['error', 2],
+    'linebreak-style': ['error', 'windows'],
+    quotes: ['error', 'single'],
+    semi: ['error', 'never'],
+    'react/react-in-jsx-scope': 'off',
+    eqeqeq: 'error',
+    'no-trailing-spaces': 'error',
+    'object-curly-spacing': ['error', 'always'],
+    'arrow-spacing': ['error', { before: true, after: true }],
+    'react/prop-types': 0,
+    'no-console': 0
+  },
+  settings: {
+    react: {
+      version: 'detect'
+    }
+  }
+}
