b81af6125f134962cbb6a0ddb33122c7ba34c178
juhopaakkanen <juho.paakkanen@gmail.com>
2022-07-08 16:51:22 +0300

    Clean folder structure and refactor code

--- part9/Patientor_frontend/src/utils/helpers.ts
+++ part9/Patientor_frontend/src/utils/helpers.ts
@@ -2,15 +2,15 @@ export const isDate = (date: string): boolean => {
   return Boolean(Date.parse(date));
 };
 
-export const assertNever = (value: never): never => {
-  throw new Error(
-    `Unhandled discriminated union member: ${JSON.stringify(value)}`
-  );
-};
-
 export const isOptionalDate = (date: string): boolean => {
   if (date === "") {
     return true;
   }
   return Boolean(Date.parse(date));
+};
+
+export const assertNever = (value: never): never => {
+  throw new Error(
+    `Unhandled discriminated union member: ${JSON.stringify(value)}`
+  );
 };
